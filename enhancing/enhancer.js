246d2335afee642caf7130cd899b2b066aaa4094
Michael Brady <mikebrady44@gmail.com>
2020-06-24 18:25:55 -0400

    Completed base assignment and stretch assignment

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return { ...item, enhancement: Math.min(item.enhancement + 1, 20) };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement >= 15) {
+    if (item.enhancement === 15) {
+      return { ...item, durability: Math.max(item.durability - 10, 0) };
+    } else {
+      return {
+        name: item.name,
+        durability: Math.max(item.durability - 10, 0),
+        enhancement: Math.max(item.enhancement - 1, 0),
+      };
+    }
+  } else {
+    return { ...item, durability: Math.max(item.durability - 5, 0) };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  const repairedItem = { ...item, durability: 100 };
+  return repairedItem;
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement === 0) {
+    return { ...item };
+  } else {
+    const name = "[+" + item.enhancement + "] " + item.name;
+    return { ...item, name };
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,142 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+it("checks that jest is setup", function () {
+  expect(true).toBe(true);
+});
+
+describe("enhancer", function () {
+  it("Test repair function sets durability to 100", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 18,
+    };
+    const repairedItem = enhancer.repair(item);
+    expect(repairedItem.durability).toBe(100);
+  });
+
+  it("success function increments enhancement prop by 1 if <20", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 18,
+    };
+    const success = enhancer.succeed(item);
+    expect(success).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 50,
+        enhancement: 19,
+      })
+    );
+  });
+
+  it("success function does nothing if enhancement = 20", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 20,
+    };
+    const success = enhancer.succeed(item);
+    expect(success).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 50,
+        enhancement: 20,
+      })
+    );
+  });
+
+  it("fail decreases durability by 5 if enhancement <15", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 12,
+    };
+    expect(enhancer.fail(item)).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 45,
+        enhancement: 12,
+      })
+    );
+  });
+
+  it("fail decreases durability by 10 if enhancement is >=15", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 15,
+    };
+    expect(enhancer.fail(item)).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 40,
+        enhancement: 15,
+      })
+    );
+  });
+
+  it("fail won't decrease beyond 0", function () {
+    const item = {
+      name: "wario",
+      durability: 5,
+      enhancement: 15,
+    };
+    expect(enhancer.fail(item)).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 0,
+        enhancement: 15,
+      })
+    );
+  });
+
+  it("fail decreases enahancement by 1 if enhancement >16", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 16,
+    };
+    expect(enhancer.fail(item)).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 40,
+        enhancement: 15,
+      })
+    );
+  });
+});
+
+describe("get function", function () {
+  it("get modifies the name if enhancement > 0", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 15,
+    };
+    expect(enhancer.get(item)).toEqual(
+      expect.objectContaining({
+        name: "[+15] wario",
+        durability: 50,
+        enhancement: 15,
+      })
+    );
+  });
+
+  it("get does not modify name if enhancecment === 0", function () {
+    const item = {
+      name: "wario",
+      durability: 50,
+      enhancement: 0,
+    };
+    expect(enhancer.get(item)).toEqual(
+      expect.objectContaining({
+        name: "wario",
+        durability: 50,
+        enhancement: 0,
+      })
+    );
+  });
+});
